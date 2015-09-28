dedfa80dfdabd376afe247200f798c5b1a321910
RazZa <eldiaman@csd.uoc.gr>
2015-09-28 14:47:56 +0200

    many beries

--- main.lua
+++ main.lua
@@ -2,21 +2,33 @@ local _W = display.viewableContentWidth
 local _H = display.viewableContentHeight
 local soundID = audio.loadSound( "froot.wav" )
 local background = display.newImage("bg.jpg", display.contentCenterX, display.contentCenterY);
-local froot = "froot.png";
-local view = display.newImage(froot);
+local file_name = "froot.png";
+local halfW = display.viewableContentWidth / 2
+local halfH = display.viewableContentHeight / 2
 
-view.x=_W/5
-view.y=_H/2
+
+local unview = function( event )
+	if event.phase=="began" then
+		print ("TOUCHED")
+		--view.isVisible=false
+		--audio.play(soundID)
+		return true
+	end
+end
+
 
 local physics = require ("physics")
 physics.start();
-borderBodyElement = { density = 5.0, friction = 0.1, bounce = 0.7 }
-physics.addBody(view,"dynamic",{density=1.0, friction=0.9, bounce=0.3})
-view:applyForce(5000,50,view.x,view.y)
 
+
+
+
+---- Border initialization -----
+borderBodyElement = { density = 5.0, friction = 0.1, bounce = 0.7 }
 local borderTop = display.newRect( -50, 0, 1, display.viewableContentHeight*2 )
 --borderTop:setFillColor( 0, 0, 0, 0)		-- make invisible
 physics.addBody( borderTop, "static", borderBodyElement )
+
 
 local borderBottom = display.newRect( 0, 320, 2000, 1 ) --bot
 --borderBottom:setFillColor( 0, 0, 0, 0)		-- make invisible
@@ -30,16 +42,17 @@ local borderRight = display.newRect( 550, 250, 1, 500 ) --right border
 --borderRight:setFillColor( 0, 0, 0, 0)		-- make invisible
 physics.addBody( borderRight, "static", borderBodyElement )
 
-function view:touch(event)
-	if event.phase=="began" then
-		print ("TOUCHED")
-		view.isVisible=false
-		--audio.play(soundID)
-		return true
-	end
-end
+for i=1,5 do
+	local view = display.newImage(file_name);
+	view:translate( halfW + math.random( -100, 100 ), halfH + math.random( -100, 100 ) )
+	--view:applyForce(math.random(-100, 100), 50, view.x, view.y)
+	view.vx = math.random( 1, 5 )
+	view.vy = math.random( -2, 2 )
+	physics.addBody(view,"dynamic",{density=1.0, friction=0.9, bounce=0.3})
+	view:addEventListener( "touch", unview )
+end    
 
-view:addEventListener("touch",view)
+
 
 
 
